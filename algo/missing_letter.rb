d0266a1ab17d8968c9c574056f77d6ac6b5e8652
Tom Yang <xy2286@columbia.edu>
2016-08-05 21:22:25 -0400

    algo solved

--- algo/missing_letter.rb
+++ algo/missing_letter.rb
@@ -3,7 +3,19 @@
 # bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd", write your own test.
 
 def find_missing_letter(range)
-
+  string_split = range.split("")
+  letter_range = (string_split[0]..string_split[-1])
+  result = ""
+  letter_range.to_a.each do |letter|
+    unless range.include?(letter)
+      puts letter
+      result += letter
+    end
+  end
+  if result == ""
+    result = nil
+  end
+  return result
 end
 
 # Driver code - don't touch anything below this line.
@@ -29,6 +41,18 @@ puts result
 puts
 
 if result == nil
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = find_missing_letter("acdgh")
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "bef"
   puts "PASS!"
 else
   puts "F"
